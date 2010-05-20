ddf110f50fb64c59a95140f305ef040056714b47
Amit Rathore <amitrathore@gmail.com>
2010-05-20 16:09:28 -0700

    using apache pool library from clojars

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/ragnard/redis-clojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.apache/commons-pool "1.5.4"]]
+                 [commons-pool "1.5.4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
